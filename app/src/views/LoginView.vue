78b5397f35666435e2432ac442cc350006527e0a
quentin pimont <quentin.pimont.60@gmail.com>
2023-07-09 16:27:21 +0200

    create web app

---
+++ app/src/views/LoginView.vue
@@ -0,0 +1,9 @@
+<script setup>
+    import { useCounterStore } from '../stores/counter'
+
+    const store = useCounterStore();
+</script>
+
+<template>
+    <button @click="store.increment">click {{ store.$state.count }}</button>
+</template>
