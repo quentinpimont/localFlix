4805a7b16e4e27e91da209900a230520101f6552
quentin <quentin.pimont.60@gmail.com>
2023-07-11 20:24:22 +0200

    Add same origin header for request

--- api/index.js
+++ api/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const routes = require('./lib/application/index');
 const { isAuth } = require('./lib/middleWares/authMiddleWare');
 const app = express();
@@ -12,7 +11,13 @@ const errorHandling = (err, req, resp, next) => {
     next(err);
 }
 
-app.use(bodyParser.json());
+app.use(express.json());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.get('/api', (req, res) => {
     res.json({
